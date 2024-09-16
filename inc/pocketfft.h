77f4fe3d5b9aa1c874de9d020f6d28b9f1a22dfa
Jake Zeisel <jacobzeisel@gmail.com>
2024-09-16 16:50:40 -0500

    add pocket fft

---
+++ inc/pocketfft.h
@@ -0,0 +1,19 @@
+#ifndef POCKETFFT_H_
+#define POCKETFFT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "csoundlib.h"
+
+
+//
+// TODO: 2D fft
+//
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // POCKETFFT_H_
